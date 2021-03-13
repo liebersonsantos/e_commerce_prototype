fd2817ab2c4a194557bf1efe0be5beb9870ee480
liebersonsantos <lieberson@lieberson-2.local>
2021-03-13 20:15:35 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/liebersonsantos/e_commerce_prototype/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.liebersonsantos.e_commerce_prototype
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:e_commerce_prototype/contants.dart';
+import 'package:e_commerce_prototype/screens/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: "Muli",
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: kTextColor),
+          bodyText2: TextStyle(color: kTextColor),
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
