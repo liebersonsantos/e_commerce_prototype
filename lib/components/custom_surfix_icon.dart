d64d645d9893efee2dad4c021c93f9e26328f36e
liebersonsantos <lieberson@lieberson-2.local>
2021-03-14 21:13:30 -0300

    signup screen with error handling and scrollview

--- lib/components/custom_surfix_icon.dart
+++ lib/components/custom_surfix_icon.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../size_config.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -1,8 +1,13 @@
 import 'package:e_commerce_prototype/components/custom_surfix_icon.dart';
+import 'package:e_commerce_prototype/components/default_button.dart';
+import 'package:e_commerce_prototype/components/form_error.dart';
+import 'package:e_commerce_prototype/components/social_icon.dart';
 import 'package:e_commerce_prototype/contants.dart';
+import 'package:e_commerce_prototype/screens/sign_in/components/sign_form.dart';
 import 'package:e_commerce_prototype/size_config.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -13,51 +18,63 @@ class Body extends StatelessWidget {
         child: Padding(
           padding:
               EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-          child: Column(
-            children: [
-              Text(
-                "Welcome back",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: getProportionateScreenWidth(28),
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.04),
+                Text(
+                  "Welcome back",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                "Sign in with your email and password \nor continue with social media",
-                textAlign: TextAlign.center,
-              ),
-              SingForm(),
-            ],
+                Text(
+                  "Sign in with your email and password \nor continue with social media",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                SingForm(),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocialCard(
+                      icon: "assets/icons/google-icon.svg",
+                      press: () {},
+                    ),
+                    SocialCard(
+                      icon: "assets/icons/facebook-2.svg",
+                      press: () {},
+                    ),
+                    SocialCard(
+                      icon: "assets/icons/twitter.svg",
+                      press: () {},
+                    ),
+                  ],
+                ),
+                SizedBox(height: getProportionateScreenHeight(20),),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account? ",
+                      style: TextStyle(fontSize: getProportionateScreenWidth(16)),
+                    ),
+                    Text(
+                      "Sign Up",
+                      style: TextStyle(
+                          fontSize: getProportionateScreenWidth(16),
+                          color: kPrimaryColor),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-class SingForm extends StatefulWidget {
-  @override
-  _SingFormState createState() => _SingFormState();
-}
-
-class _SingFormState extends State<SingForm> {
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        children: [
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: "Email",
-              hintText: "Enter your email",
-              floatingLabelBehavior: FloatingLabelBehavior.always,
-              suffixIcon: CustomSuffixIcon(svgIcon: "assets/icons/Mail.svg"),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
